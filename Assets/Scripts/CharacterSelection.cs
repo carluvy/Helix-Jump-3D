103c49b7ebd8decef92618b4e5c9487845dd3a4b
Carla Aluvai <carngie@gmail.com>
2022-10-12 22:32:26 +0300

    Added character selection feature

---
+++ Assets/Scripts/CharacterSelection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class CharacterSelection : MonoBehaviour
+{
+    public GameObject[] characters;
+    private int selectedCharacter = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(GameObject ch in characters)
+        {
+            ch.SetActive(false);
+
+        }
+        characters[selectedCharacter].SetActive(true);
+    }
+
+    public void ChangeCharacter(int newCharacter)
+    {
+        characters[selectedCharacter].SetActive(false);
+        characters[newCharacter].SetActive(true);
+        selectedCharacter = newCharacter;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
