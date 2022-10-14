1f31f1703bffb53a903ca93d5de92dec4e4d6182
Carla Aluvai <carngie@gmail.com>
2022-10-14 22:20:22 +0300

    added touch functionality

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
-    public float rotationSpeed = 150;
+    public float rotationSpeed = 100;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +14,30 @@ public class Rotator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (!GameManager.isGameStarted)
+            return;
+        #region standalone inputs
         if (Input.GetMouseButton(0))
         {
+            
             float mouseX = Input.GetAxisRaw("Mouse X");
             transform.Rotate(0, -mouseX * rotationSpeed * Time.deltaTime, 0);
+
         }
-        
+        #endregion
+
+        #region touch inputs
+
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) 
+        {
+            float xDelta = Input.GetTouch(0).deltaPosition.x;
+            transform.Rotate(0, -xDelta * rotationSpeed * Time.deltaTime, 0);
+
+        }
+
+
+        #endregion
+
     }
 }
